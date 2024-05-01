3334ba3156b0e7d0eba3c935234fa98037dd9c0c
Jonathan Costa <jhoncosta2@gmail.com>
2024-05-01 19:12:48 -0300

    Primeira aula anatomia das classes Java

---
+++ bootCampBackEndJavaSantander/anatomia_classes/src/MinhaClasse.java
@@ -0,0 +1,5 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Curso de BackEnd Java");
+    }
+}
---
+++ bootCampBackEndJavaSantander/primeiroProjetoJava/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Primeiro Programa do BootCamp Back-End com Java!");
+    }
+}
