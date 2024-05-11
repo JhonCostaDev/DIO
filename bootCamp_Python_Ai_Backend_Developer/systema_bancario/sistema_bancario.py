96fbcdc748f60962327b08e36e53af122353ad16
Jonathan Costa <jhoncosta2@gmail.com>
2024-05-11 15:11:01 -0300

    Correção gramatical e ajustes finais no desafio Sistema Bancário

--- bootCamp_Python_Ai_Backend_Developer/systema_bancario/sistema_bancario.py
+++ bootCamp_Python_Ai_Backend_Developer/systema_bancario/sistema_bancario.py
@@ -1,58 +1,53 @@
-saldo_usuario = 10
-saques_diario = 3
-limite_por_saque = 500
-registro_saques_depositos = ""
+# DECLARAÇÃO DAS VARIÁVEIS GLOBAIS
+saldo_usuario = 0       # Saldo.
+saques_diario = 3       #Limite de saques diário.
+limite_por_saque = 500  # Limite de valor por saque.
+extrato = ""            # Registro das movimentações do usuário.
+contador_operacoes = 1  # Contador de operações, será utilizado no extrato.
 
-''''
-def sacar():
-	valor = float(input("Digite o valor do saque:\n"))
-	if saques_diario == 0:
-		print("Voce atingiu o limite maximo de saques por dia, tente novamente amanha!")
-	elif valor > saldo_usuario:
-		print("Saldo insuficiente")
-	elif valor > limite_por_saque:
-		print("Limite de saque diario(R$ 500,00) excedido!\nTente outro valor")
-	else:
-		saldo_usuario -= valor
-		saques_diario -= 1
-'''
-
+# MENU de OPÇÔES
 menu = """
 #############################
-	Bem vindo ao
-	Python Bank
+	    Bem vindo ao
+	     Python Bank
 #############################
 Escolha uma das opcao abaixo
 
-[1] - Sacar
-[2] - Depositar
-[3] - Extrato
-[0] - Finalizar
+        [1] - Sacar
+        [2] - Depositar
+        [3] - Extrato
+        [0] - Finalizar
 
 #############################
 
 """
-atendimento = True
+
+# LOOP WHILE TRUE DE OPÇÕES
+atendimento = True# CONTROLARÁ O LOOP ATÉ A MESMA SER DECLARADA COMO FALSE
 while atendimento:
     operacao = input(menu)
 
+# SAQUE
     if operacao == "1":
-        print("Sacar")
+        print("######### Saque #########")
         valor = float(input("Digite o valor do saque:\n"))
-
-        if saques_diario == 0:
-            print("Voce atingiu o limite maximo de saques por dia, tente novamente amanha!")
-        elif valor > saldo_usuario:
+        if valor < 0: #                         #Verifica se o valor digitado é negativo.
+            print("Valor Inválido!")
+        elif saques_diario == 0:                #Verifica se o limiti diário de saque foi atingido.
+            print("Você atingiu o limite máximo de saques por dia, tente novamente amanhã!")
+        elif valor > saldo_usuario:             #Verifica se o cliente tem saldo suficiente na conta
             print("Saldo insuficiente")
-        elif valor > limite_por_saque:
+        elif valor > limite_por_saque:          #Verifica se o valor solicitado ultrapassa o limite por saque
             print("Limite de saque diario(R$ 500,00) excedido!\nTente outro valor")
-        else:
-            saldo_usuario -= valor
-            saques_diario -= 1
-            registro_saques_depositos += f"Saque: R${valor:.2f}\n"
+        else:                                   # Passados todas as verificações realizará a operação
+            saldo_usuario -= valor              # Debita o valor solicitado da conta do usuário.
+            saques_diario -= 1                  #
+            extrato += f"{contador_operacoes} - Saque no valor de: R${valor:.2f}\n"
+            contador_operacoes += 1
             print(f"Saque no valor de {valor:.2f} Realizado!\nRetire o valor abaixo!")
 
-            outra_operacao = input("""
+            # Sub-Menu para verificar se o usuário deseja fazer outro procedimento ou sair do sistema
+            outra_operacao = input(""" 
 Deseja Realizar outra operacao?
 		[1]- SIM
 		[0]-NAO
@@ -65,22 +60,25 @@ Deseja Realizar outra operacao?
                 continue
 
 
+# DEPOSITO
     elif operacao == "2":
-        print("Deposito")
-        deposito = float(input("Qual o valor R$ do depósito: \n"))
+        print("######### Deposito #########")
+        deposito = float(input("Qual o valor R$ do depósito: \n")) # Coleta o valor solicitado
 
-        if deposito < 0:
+        if deposito < 0:    # Verifica se o valor é válido
             print("Valor inválido")
             print("Tente Novamente!")
+        else:                               # Realiza a operação de depósito
+            saldo_usuario += deposito       # Adiciona o valor a conta.
+            extrato += f"{contador_operacoes} - Depósito no valor de R${deposito:.2f}\n" # Registra a operação no extrato.
+            contador_operacoes += 1         # Registra a operação.
+            print(f"Depósito no valor de R${deposito:.2f}\nRealizado com sucesso!")
 
-        saldo_usuario += deposito
-        registro_saques_depositos += f"Depósito no valor de R${deposito:.2f}\n"
-        print(f"Depósito no valor de R${deposito:.2f} realizado com sucesso!")
-
+        # Sub-Menu para verificar se o usuário deseja fazer outro procedimento ou sair do sistema
         outra_operacao = input("""
-        Deseja Realizar outra operacao?
-        		[1]- SIM
-        		[0]-NAO
+Deseja Realizar outra operacao?
+		   [1] - SIM
+		   [0] - NAO
         			""")
 
         if outra_operacao == "0":
@@ -89,25 +87,27 @@ Deseja Realizar outra operacao?
         else:
             continue
 
-
+# EXTRATOS
     elif operacao == "3":
-        print("Extrato")
-        print(f"Últimas Movimentações\n{registro_saques_depositos}")
-
-        print(f"Saldo atual é de: {saldo_usuario:.2f}")
+        print("########### Extrato ###########")    #Impressão do extrato.
+        print("          Movimentações")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"Saldo atual é de: R${saldo_usuario:.2f}")
 
         outra_operacao = input("""
-                Deseja Realizar outra operacao?
-                		[1]- SIM
-                		[0]-NAO
+Deseja Realizar outra operacao?
+		   [1] - SIM
+		   [0] - NAO
                 			""")
 
+        # Sub-Menu para verificar se o usuário deseja fazer outro procedimento ou sair do sistema
         if outra_operacao == "0":
             print("Obrigado por utilizar nossos servicos")
             atendimento = False
         else:
             continue
 
+# QUEBRA DO LOOP
     elif operacao == "0":
         print("Obrigado por utilizar nossos servicos")
         atendimento = False
@@ -115,4 +115,4 @@ Deseja Realizar outra operacao?
     else:
         print("Opcao invalida, Tente Novamente!")
 
-print(registro_saques_depositos)
+# FIM DO PROGRAMA.
