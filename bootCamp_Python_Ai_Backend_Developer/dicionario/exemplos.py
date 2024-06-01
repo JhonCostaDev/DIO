11856c2dfebfe5617aaebe39bbd58f23f3f74400
Jonathan Costa <jhoncosta2@gmail.com>
2024-06-01 16:05:00 +0000

    conclusão dicionários

--- bootCamp_Python_Ai_Backend_Developer/dicionario/exemplos.py
+++ bootCamp_Python_Ai_Backend_Developer/dicionario/exemplos.py
@@ -1,19 +1,24 @@
-contatos = {
-    "joaozinho@gmail.com" : {"nome" : "joao", "telefone" : "3398-4587"},
-    "pedrinho@gmail.com" : {"nome" : "pedro", "telefone" : "3398-9863"},
-    "sofi@gmail.com" : {"nome" : "sophia", "telefone" : "3398-0247"},
-    "gabizinha@gmail.com" : {"nome" : "Gabriella", "telefone" : "3398-3620"},
-}
+# contatos = {
+#     "joaozinho@gmail.com" : {"nome" : "joao", "telefone" : "3398-4587"},
+#     "pedrinho@gmail.com" : {"nome" : "pedro", "telefone" : "3398-9863"},
+#     "sofi@gmail.com" : {"nome" : "sophia", "telefone" : "3398-0247"},
+#     "gabizinha@gmail.com" : {"nome" : "Gabriella", "telefone" : "3398-3620"},
+# }
 
-# Acessando campos
+# # Acessando campos
 
-telefone = contatos["pedrinho@gmail.com"]["telefone"] # "3398-9863"
-print(telefone)
+# telefone = contatos["pedrinho@gmail.com"]["telefone"] # "3398-9863"
+# print(telefone)
 
-## Iterar dicionários
+# ## Iterar dicionários
 
-#for chave in contatos:
-    #print(chave, contatos[chave])
+# #for chave in contatos:
+#     #print(chave, contatos[chave])
     
-for chave, valor in contatos.items():
-    print(chave, valor)
+# for chave, valor in contatos.items():
+#     print(chave, valor)
+
+carro = {"marca": "Fiat", "modelo": "palio", "placa": "ABD-9826"}
+
+motor = carro.get('motor')
+print(motor)
